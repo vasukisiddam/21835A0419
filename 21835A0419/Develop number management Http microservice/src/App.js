1e0200735369861f9e8dd07af9f30ebe4d9cf6b0
Siddam Vasuki Abhirami <vasukisiddam03@gmail.com>
2023-08-17 13:53:08 +0530

    Add files via upload

---
+++ 21835A0419/Develop number management Http microservice/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Body_details from './components/Body_details';
+
+function App() {
+  return (
+    <>
+    
+    <Navbar></Navbar>
+    <Body_details></Body_details>
+    </>
+  );
+}
+
+export default App;
