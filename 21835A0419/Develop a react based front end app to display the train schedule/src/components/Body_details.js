8501fa3d11802d8c3a4281188b30e71cbc928697
Siddam Vasuki Abhirami <vasukisiddam03@gmail.com>
2023-08-17 14:08:41 +0530

    Add files via upload

---
+++ 21835A0419/Develop a react based front end app to display the train schedule/src/components/Body_details.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Body_details() {
+  return (
+    <div>
+      <div className="card">
+  <img src="C:\Users\HP\Desktop\images\train2" className="card-img-top" alt="Humsafar"/>
+  <div className="card-body">
+    <h5 className="card-title">Humsafar</h5>
+    <p className="card-text">Humsafar Express is a fully premium service with AC-3 Tier and Sleeper class accommodation having all the modern facilities designed and operated by Indian Railways.</p>
+    <a href="https://en.wikipedia.org/wiki/Humsafar_Express" className="btn btn-primary">ReadMore</a>
+  </div>
+</div>
+<br/>
+<div className="card" >
+  <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3D%2522indian%2Brailway%2522&psig=AOvVaw2KHweq-rW_rt_u0_0GF3-A&ust=1690091194685000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNDKlLzOoYADFQAAAAAdAAAAABAE" className="card-img-top" alt="Rajdhani Express"/>
+  <div className="card-body">
+    <h5 className="card-title">Rajdhani Express</h5>
+    <p className="card-text">The Rajdhani Express is a series of passenger train services in India operated by Indian Railways connecting the national capital New Delhi with the capitals or the largest cities of various states. The word Rajdhani has been derived from the Sanskrit language, which means Capital in English.</p>
+    <a href="https://en.wikipedia.org/wiki/Rajdhani_Express" className="btn btn-primary">ReadMore</a>
+  </div>
+</div>
+<br/>
+<div className="card" >
+  <img src="https://www.dreamstime.com/photos-images/train.html" className="card-img-top" alt="Sher A Punjab"/>
+  <div className="card-body">
+    <h5 className="card-title">Sher A Punjab</h5>
+    <p className="card-text">The 12497 / 98 New Delhi–Amritsar Shan-e-Punjab Express is a Superfast Express train belonging to Indian Railways – Northern Railway zone that runs between ...</p>
+    <a href="https://en.wikipedia.org/wiki/Shan-e-Punjab_Express" className="btn btn-primary">ReadMore</a>
+  </div>
+</div>
+    </div>
+  )
+}
